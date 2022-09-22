b0601b579489670b6a2194d986813fc590e44831
savethetunas <74320483+savethetunas@users.noreply.github.com>
2022-09-22 16:10:29 -0700

    Initial commit

---
+++ stock_plotter.py
@@ -0,0 +1,13 @@
+###
+### Author: Adam Fehse
+### Course: CSc 110
+### Description: A stock plotter.
+
+
+mode = '' or ''
+
+while mode != 'v' and mode != 'h':
+    mode = input('Enter mode (horizontal or vertical):\n')
+
+print(mode)
+
