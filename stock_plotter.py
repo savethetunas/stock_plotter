39fc5abf6451d4560698021f2f0bf85991d92866
savethetunas <74320483+savethetunas@users.noreply.github.com>
2022-09-22 20:30:13 -0700

    just to see

--- stock_plotter.py
+++ stock_plotter.py
@@ -7,7 +7,7 @@ mode = '' or ''
 while mode != 'horizontal' and mode != 'vertical':
     mode = input('Enter stock plotter mode:\n')
 
-stock_string = input('Enter stock string:\n')
+stock_string = input('Enter stock plot input string:\n')
 length = len(stock_string) % 2
 while length != 0:
     stock_string = input('|Enter stock plot input string:\n')
