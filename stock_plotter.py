758e8e1c61e1d78783e6d444463d0d1c320870e2
savethetunas <74320483+savethetunas@users.noreply.github.com>
2022-09-23 22:33:52 -0700

    just to see

--- stock_plotter.py
+++ stock_plotter.py
@@ -3,31 +3,31 @@
 ### Course: CSc 110
 ### Description: A stock plotter.
 
-mode = '' or ''
+mode = ''
 while mode != 'horizontal' and mode != 'vertical':
     mode = input('Enter stock plotter mode:\n')
 
-stock_string = input('Enter stock plot input string:\n')
-length = len(stock_string) % 2
+string = input('Enter stock plot input string:\n')
+length = len(string) % 2
 while length != 0:
-    stock_string = input('|Enter stock plot input string:\n')
-    length = len(stock_string) % 2
-print('####################################################################')
-print('#                                                                  #')
-print('#                                                                  #')
-print('#                                                                  #')
-print('#                                                                  #')
-print('#                                                                  #')
-print('#                                            ****   *              #')
-print('#                                           *    * * *             #')
-print('#                 ***                  *** *      *             ** #')
-print('# *          * **    **             ***   *           *        *   #')
-print('#  **       * *  *     **          *                     ***  *    #')
-print('#    *     *             **     ***                    **   **     #')
-print('#     **                       *                                   #')
-print('#       ***                *  *                                    #')
-print('#                           *                                      #')
-print('#                            *                                     #')
-print('#                                                                  #')
-print('#                                                                  #')
-print('####################################################################')
+    stock_string = input('Enter stock plot input string:\n')
+    length = len(string) % 2
+
+if mode == 'vertical':
+    print('#' * 19)
+    i = 0
+    position = 8
+    while i < len(string):
+        if string[i] == 'u':
+            print('#' + ' ' * (position + (int(string[i + 1]))) + '*' + ' ' * (
+                    16 - position - (int(string[i + 1]))) + '#')
+            position = position + (int(string[i + 1]))
+        if string[i] == 'd':
+            print('#' + ' ' * (position - (int(string[i + 1]))) + '*' + ' ' * (
+                    16 - position + (int(string[i + 1]))) + '#')
+            position = position - (int(string[i + 1]))
+        i += 1
+    if i >= len(string):
+        print('#' * 19)
+        exit()
+
