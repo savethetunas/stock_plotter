c5656f331981a1ce87af451064129e59ee47e40c
savethetunas <74320483+savethetunas@users.noreply.github.com>
2022-09-27 13:51:41 -0700

    just to see

--- stock_plotter.py
+++ stock_plotter.py
@@ -1,70 +1,68 @@
 ###
 ### Author: Adam Fehse
 ### Course: CSc 110
-### Description: A stock plotter. Can print vertical or horizontal modes.
+### Description: A stock plotter that can print vertical or horizontal charts.
 
-# This checks for and repeats until correct input.
+# Ensure input mode is correct input.
 mode = ''
 while mode != 'horizontal' and mode != 'vertical':
     mode = input('Enter stock plotter mode:\n')
 
-# Checks to ensure string is correct length>.
+# Ensure string length is multiple of two.
 string = input('Enter stock plot input string:\n')
 length = len(string) % 2
 while length != 0:
-    stock_string = input('Enter stock plot input string:\n')
+    string = input('Enter stock plot input string:\n')
     length = len(string) % 2
 
-#Sets the starting position.
-#On u's/d's prints spaces and location based on string.
-#Position 8 is the middle of the chart and the numerals [i + 1] tell how many spaces
-#to moves.
 if mode == 'vertical':
+
     print('#' * 19)
+    p = 8  # Position begins at 8. Center of chart.
     i = 0
-    position = 8
-    while i < len(string):
+    while i < len(string):  # Loops (length of string) times.
         if string[i] == 'u':
-            print('#' + ' ' * (position + (int(string[i + 1]))) + '*' + ' ' * (
-                    16 - position - (int(string[i + 1]))) + '#')
-            position = position + (int(string[i + 1]))
+            num = int(string[i + 1])  # Numeral from input string.
+            space = p + num  # Spacing left of '*'.
+            behind_space = 16 - p - num  # Other side of '*' spacing.
+            p = p + num  # Update position rightwards.
+            output = '#' + ' ' * space + '*' + ' ' * behind_space + '#'
+            print(output)
+
         if string[i] == 'd':
-            print('#' + ' ' * (position - (int(string[i + 1]))) + '*' + ' ' * (
-                    16 - position + (int(string[i + 1]))) + '#')
-            position = position - (int(string[i + 1]))
+            num = int(string[i + 1])
+            space = p - num  # Spacing to left of '*'.
+            behind_space = 16 - p + num
+            p = p - num  # Update position leftwards.
+            output = '#' + ' ' * space + '*' + ' ' * behind_space + '#'
+            print(output)
         i += 1
-    if i >= len(string): # if i ever >= than length of the string, print # * 19 and exit.
+    if i >= len(string):  # After iterating through the string, print last row, exit.
         print('#' * 19)
         exit()
 
 if mode == 'horizontal':
     length_div_two = len(string) / 2
+    top = '##' + '#' * int(length_div_two) + '##'
+    print(top)  # Top row.
 
-    print('##' + '#' * int(length_div_two) + '##')
-    rows = 16 #count backwards from 16
+    rows = 16  # Start with 16 rows.
     while rows >= 0:
-        print('#', end=' ')  # Print one '#', stay on the row.
-        to_print = ""
+        print('#', end=' ')  # Prints left wall, ends on the same line.
         count = 0
-        level = 8
+        level = 8  # Center of the chart. Move up and down from this point.
         while count < len(string):
             if string[count] == 'u':
-                level += int(string[count + 1])
+                level += int(string[count + 1])  # Add numeral to level and save into level variable.
             else:
-                level -= int(string[count + 1])
-            if rows == level:
+                level -= int(string[count + 1])  # Minus numeral form level and save into level variable.
+            if rows == level:  # Check row and level, if they are equal print star.
                 print('*', end='')
             else:
                 print(' ', end='')
+            count += 2  # Count +2 because +1 iterates to the next place which would be an alpha.
+        output = ' ' * (int(length_div_two) - count) + " #"
+        print(output)
+        rows -= 1  # Subtract 1 from rows and restart loop.
 
-            count += 2
-
-        to_print += ' ' * (int(length_div_two) - count) + " #"
-
-        print(to_print)
-        rows -= 1
-
-    print('##' + '#' * int(length_div_two) + '##')
-
-
-
+    print(top)
