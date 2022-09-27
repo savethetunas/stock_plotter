512f09a915f0a34a0e0afdefe66983f0a27d370e
savethetunas <74320483+savethetunas@users.noreply.github.com>
2022-09-27 14:37:11 -0700

    just to see

--- stock_plotter.py
+++ stock_plotter.py
@@ -16,28 +16,28 @@ while length != 0:
     length = len(string) % 2
 
 if mode == 'vertical':
-
     print('#' * 19)
     p = 8  # Position begins at 8. Center of chart.
     i = 0
     while i < len(string):  # Loops (length of string) times.
         if string[i] == 'u':
             num = int(string[i + 1])  # Numeral from input string.
-            space = p + num  # Spacing left of '*'.
-            behind_space = 16 - p - num  # Other side of '*' spacing.
-            p = p + num  # Update position rightwards.
+            space = p + num  # Space left of '*'.
+            behind_space = 16 - p - num  # Space right of '*'.
+            p = p + num  # Update position right.
             output = '#' + ' ' * space + '*' + ' ' * behind_space + '#'
             print(output)
 
         if string[i] == 'd':
             num = int(string[i + 1])
-            space = p - num  # Spacing to left of '*'.
+            space = p - num  # Space left of '*'.
             behind_space = 16 - p + num
-            p = p - num  # Update position leftwards.
+            p = p - num  # Update position left.
             output = '#' + ' ' * space + '*' + ' ' * behind_space + '#'
             print(output)
         i += 1
-    if i >= len(string):  # After iterating through the string, print last row, exit.
+
+    if i >= len(string):  # Print last row, exit.
         print('#' * 19)
         exit()
 
@@ -50,17 +50,17 @@ if mode == 'horizontal':
     while rows >= 0:
         print('#', end=' ')  # Prints left wall, ends on the same line.
         count = 0
-        level = 8  # Center of the chart. Move up and down from this point.
+        level = 8  # Center of the chart. Move up and down from here.
         while count < len(string):
             if string[count] == 'u':
-                level += int(string[count + 1])  # Add numeral to level and save into level variable.
+                level += int(string[count + 1])  # Add numeral to level.
             else:
-                level -= int(string[count + 1])  # Minus numeral form level and save into level variable.
+                level -= int(string[count + 1])  # Minus numeral form level.
             if rows == level:  # Check row and level, if they are equal print star.
                 print('*', end='')
             else:
-                print(' ', end='')
-            count += 2  # Count +2 because +1 iterates to the next place which would be an alpha.
+                print(' ', end='')  # Print empty space. Stay on same line.
+            count += 2  # Count +2 because +1 iterates to alpha char.
         output = ' ' * (int(length_div_two) - count) + " #"
         print(output)
         rows -= 1  # Subtract 1 from rows and restart loop.
